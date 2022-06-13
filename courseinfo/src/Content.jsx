6c20de6e91da47d7c0df47a93ea845fe49115b1f
skvoren <skvoren@outlook.com>
2022-06-13 13:58:38 +0400

    exercise 2.2

--- courseinfo/src/Content.jsx
+++ courseinfo/src/Content.jsx
@@ -3,11 +3,22 @@ import Part from "./Part";
 
 const Content = ({parts}) => {
 
+    const TotalExercises = (parts) => {
+        let result = 0
+
+        for (let i = 0; i < parts.length; i++) {
+            result+= parts[i].exercises
+        }
+
+        return result
+    } 
+
     return (
         <div>
             {parts.map((part) =>
                 <Part key={part.id} part={part}/>
             )}
+            <p>total of {TotalExercises(parts)} exercises</p>
         </div>
     )
 }
